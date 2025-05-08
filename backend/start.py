aba02dbcd2185145ccce643524b2399eb60e30d7
ImTommyDev <tommy.devlpr@gmail.com>
2025-05-08 10:13:19 +0200

    Se ha cambiado el puerto para recibir el que railway me de porque no está funcionando a la hora de hacer el deploy

--- backend/start.py
+++ backend/start.py
@@ -3,5 +3,5 @@ import uvicorn
 
 # Creo un archivo de arranque para railway, que es el que ejecuta el servidor
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000)) 
+    port = int(os.environ["PORT"]) #Railway me da el puerto en el que tengo que ejecutar el servidor, lo guardo en una variable
     uvicorn.run("api.api:app", host="0.0.0.0", port=port) #0.0.0.0 es para que el servidor sea accesible desde cualquier IP, y el puerto es el que me da railway.
