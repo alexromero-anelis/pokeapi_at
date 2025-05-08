9e5becee6bf5d0e00c9900a06b4863e34476dd75
ImTommyDev <tommy.devlpr@gmail.com>
2025-05-08 07:39:07 +0200

    Prueba con requirements simplificado para ver si funciona railway

--- backend/api/api.py
+++ backend/api/api.py
@@ -4,19 +4,20 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# CORS config para permitir peticiones desde mi frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
-    allow_credentials=True,
+    allow_origins=["*"], #Aquí puedo especificar los dominios permitidos
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Modelo de Pokemon
 class Pokemon(BaseModel):
     id: int
     name: str
     type: str
-    
+
 pokemons = [
     Pokemon(id=1, name="Pikachu", type="Electric"),
     Pokemon(id=2, name="Charmander", type="Fire"),
@@ -26,5 +27,5 @@ pokemons = [
 ]
 
 @app.get("/pokemons")
-async def root():
+async def get_pokemons():
     return pokemons
